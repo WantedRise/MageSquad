9aad285d5175de3febc30c5462b247f03d5f684c
Juneu <corma642@naver.com>
2025-12-05 10:38:45 +0900

    NEW : 프로젝트 기본 세팅 & 플러그인 추가 & 모듈 추가 & DefaultEngine.ini 설정 추가
    추가된 세팅 작업 Jira 링크: https://magesquad.atlassian.net/browse/RISE-4
    
    프로젝트 기본 세팅
     - 시작 맵 설정 (빈 레벨)
     - 프로젝트 공개 폴더 경로 추가
     - GAS Tag 추가 파일 추가
     - Number of Players -> 4로 설정 (시작 플레이어 수)
     - - 에디터 개인설정에서 Run Under One Process를 비활성화해야 시작 시, 시작 시 설정한 플레이어 수만큼 클라이언트가 시작됨.
     - Net Mode -> Play As Listen Server 변경
    
    플러그인 추가
     - Motion Warping
     - Skeletal Merging
     - Gameplay Abilities
     - Common UI Plugin
     - UMG ViewModel
    
    모듈 추가
    
    DefaultEngine.ini 설정 추가
     - Steam 관련 설정 추가

---
+++ Source/MageSquad/MS_GameplayTags.cpp
@@ -0,0 +1,10 @@
+
+#include "MS_GameplayTags.h"
+
+namespace MS_GameplayTags
+{
+	/* Input Tags */
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+
+
+}
---
+++ Source/MageSquad/MS_GameplayTags.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace MS_GameplayTags
+{
+	/* Input Tags */
+	MAGESQUAD_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+
+
+}
