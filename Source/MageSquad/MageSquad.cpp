8490fc7fe671f1ca533198e3fe042231b4c968d5
Juneu <corma642@naver.com>
2025-12-05 09:35:55 +0900

    NEW : 새 프로젝트 생성

---
+++ Source/MageSquad/MageSquad.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MageSquad.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MageSquad, "MageSquad" );
