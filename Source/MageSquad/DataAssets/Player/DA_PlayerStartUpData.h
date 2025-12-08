bb09f61f40e06e62d3e1aaa4e6686a5205ca5926
Juneu <corma642@naver.com>
2025-12-08 17:40:25 +0900

    NEW: 플레이어 시작 데이터 에셋 클래스 추가/구현
    NEW: 플레이어 시작 데이터 에셋 클래스 추가/구현
     - 플레이어가 게임 시작 시 부여받는 기본 어빌리티 / 이펙트를 정의하는 데이터 에셋
     - 플레이어 시작 어빌리티 / 게임플레이 이펙트 구조체 선언
     - - 플레이어가 소유하게 될 UGameplayAbility 클래스 목록
     - - 시작 시 한 번 적용되는 UGameplayEffect 클래스 목록

--- Source/MageSquad/DataAssets/Player/DA_PlayerStartUpData.h
+++ Source/MageSquad/DataAssets/Player/DA_PlayerStartUpData.h
@@ -7,31 +7,39 @@
 #include "DA_PlayerStartUpData.generated.h"
 
 /**
+ * 플레이어 시작 어빌리티 / 게임플레이 이펙트 구조체
+ * Abilities : 플레이어가 소유하게 될 UGameplayAbility 클래스 목록
+ * Effects   : 시작 시 한 번 적용되는 UGameplayEffect 클래스 목록
+ */
+USTRUCT(BlueprintType)
+struct MAGESQUAD_API FStartAbilityData
+{
+	GENERATED_BODY()
+
+public:
+	// 플레이어가 시작 시 부여받는 어빌리티 목록
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GAS | StartUp")
+	TArray<TSubclassOf<class UGameplayAbility>> Abilties;
+
+	// 플레이어가 시작 시 한 번 적용되는 게임플레이 이펙트 목록
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GAS | StartUp")
+	TArray<TSubclassOf<class UGameplayEffect>> Effects;
+};
+
+/**
+ * 작성자: 김준형
+ * 작성일: 25/12/08
+ *
  * 플레이어 시작 데이터 에셋 클래스
- * 시작 시, 부여/적용되는 게임플레이 어빌리티/이펙트를 저장
+ * 플레이어가 게임 시작 시 부여받는 기본 어빌리티 / 이펙트를 정의하는 데이터 에셋
  */
 UCLASS()
 class MAGESQUAD_API UDA_PlayerStartUpData : public UDataAsset
 {
 	GENERATED_BODY()
-	
+
 public:
-	// 어빌리티 부여 함수
-	virtual void GiveToAbilitySystemComponent(class UMSPlayerAbilitySystemComponent* InASCToGive);
-
-protected:
-	// 어빌리티 부여 함수
-	void GrantAbilities(const TArray<TSubclassOf<class UGameplayAbility>>& InAbilities, class UMSPlayerAbilitySystemComponent* InASCToGive);
-
-	// 시작 시 부여 및 활성화(OnGive) 어빌리티
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Custom | StartUpData")
-	TArray<TSubclassOf<class UGameplayAbility>> ActivateOnGiveAbilities;
-
-	// 시작 시 부여(OnTriggered) 어빌리티
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Custom | StartUpData")
-	TArray<TSubclassOf<class UGameplayAbility>> ReactiveAbilities;
-
-	// 시작 시 적용 게임플레이 이펙트
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Custom | StartUpData")
-	TArray<TSubclassOf<class UGameplayEffect>> StartUpGameplayEffects;
+	// 플레이어 시작 어빌리티/이펙트 데이터
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS | StartUp")
+	FStartAbilityData PlayerStartAbilityData;
 };
