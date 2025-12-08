b1140052ed7e5ff11a15860366c86ebcf52fcfb1
Juneu <corma642@naver.com>
2025-12-08 11:27:31 +0900

    NEW: 플레이어 캐릭터 클래스 추가

---
+++ Source/MageSquad/Player/MS_PlayerController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MS_PlayerController.generated.h"
+
+/**
+ * 작성자: 김준형
+ * 작성일: 25/12/08
+ *
+ * 플레이어 컨트롤러 클래스
+ * HUD 생성 및 유지 관리
+ * 네 명의 플레이어를 모두 추척하고 속성 업데이트를 UI의 해당 슬롯에 바인딩
+ */
+UCLASS()
+class MAGESQUAD_API AMS_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+};
