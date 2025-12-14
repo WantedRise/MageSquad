640edf595c36dc1d9e043423d7846b1fa4c4509c
Juneu <corma642@naver.com>
2025-12-14 23:10:02 +0900

    NEW: 플레이어 자동 발사 어빌리티 구현
    NEW: 플레이어 자동 발사 어빌리티 구현
     - 테스트를 위해 블루프린트로 구현, 이후 C++로 변환 예정

--- Source/MageSquad/DataAssets/Player/DA_PlayerStartUpData.h
+++ Source/MageSquad/DataAssets/Player/DA_PlayerStartUpData.h
@@ -4,27 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "Types/MageSquadTypes.h"
 #include "DA_PlayerStartUpData.generated.h"
-
-/**
- * 플레이어 시작 어빌리티 / 게임플레이 이펙트 구조체
- * Abilities : 플레이어가 소유하게 될 UGameplayAbility 클래스 목록
- * Effects   : 시작 시 한 번 적용되는 UGameplayEffect 클래스 목록
- */
-USTRUCT(BlueprintType)
-struct MAGESQUAD_API FStartAbilityData
-{
-	GENERATED_BODY()
-
-public:
-	// 플레이어가 시작 시 부여받는 어빌리티 목록
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GAS | StartUp")
-	TArray<TSubclassOf<class UGameplayAbility>> Abilties;
-
-	// 플레이어가 시작 시 한 번 적용되는 게임플레이 이펙트 목록
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GAS | StartUp")
-	TArray<TSubclassOf<class UGameplayEffect>> Effects;
-};
 
 /**
  * 작성자: 김준형
@@ -40,6 +21,6 @@ class MAGESQUAD_API UDA_PlayerStartUpData : public UDataAsset
 
 public:
 	// 플레이어 시작 어빌리티/이펙트 데이터
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GAS | StartUp")
-	FStartAbilityData PlayerStartAbilityData;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Custom | StartUp")
+	FPlayerStartAbilityData PlayerStartAbilityData;
 };
