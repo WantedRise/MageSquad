616ca901b6f2d4e8f6a35135cb49f868cd3ad8f5
Juneu <corma642@naver.com>
2025-12-14 23:09:43 +0900

    CNG: 발사체 구조 변경
    CNG: 발사체 구조 변경
     - 발사체 데이터 구조체를 만들어, 발사체마다 가지는 데이터 블루프린트를 생성
     - 발사체 데이터에서 발사체와 관련된 모든 데이터를 할당 (대미지, 피해 범위, 속도, VFX 등등)

--- Source/MageSquad/MSFunctionLibrary.cpp
+++ Source/MageSquad/MSFunctionLibrary.cpp
@@ -4,6 +4,7 @@
 #include "MSFunctionLibrary.h"
 #include "AbilitySystem/ASC/MSPlayerAbilitySystemComponent.h"
 #include "AbilitySystemBlueprintLibrary.h"
+#include "Actors/Projectile/MSBaseProjectile.h"
 
 #include "MSGameplayTags.h"
 
@@ -28,4 +29,26 @@ bool UMSFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, const FGameplay
 		return ASC->HasMatchingGameplayTag(TagToCheck);
 	}
 	return false;
+}
+
+AMSBaseProjectile* UMSFunctionLibrary::LaunchProjectile(UObject* WorldContextObject, TSubclassOf<UProjectileStaticData> ProjectileDataClass, FTransform Transform, AActor* Owner, APawn* Instigator)
+{
+	// 발사한 객체의 월드 가져오기
+	UWorld* World = WorldContextObject ? WorldContextObject->GetWorld() : nullptr;
+
+	// 서버에서만 로직을 수행하도록 검사
+	if (World && World->GetNetMode() < ENetMode::NM_Client)
+	{
+		// SpawnActorDeferred 함수를 통해, 발사체 지연 생성
+		if (AMSBaseProjectile* Projectile = World->SpawnActorDeferred<AMSBaseProjectile>(AMSBaseProjectile::StaticClass(), Transform, Owner, Instigator, ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn))
+		{
+			// 발사체 정적 데이터 클래스를 초기화하고, 스폰 프로세스를 마무리
+			Projectile->ProjectileDataClass = ProjectileDataClass;
+			Projectile->FinishSpawning(Transform);
+
+			return Projectile;
+		}
+	}
+
+	return nullptr;
 }
