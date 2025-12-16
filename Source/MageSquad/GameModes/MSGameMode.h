ce89af8e393e0e8ee44de36c2d4e87b852fd0abb
LSJ <0874sj@naver.com>
2025-12-16 15:47:34 +0900

    NEW: 인게임 GameMode, GameState 클래스 추가

---
+++ Source/MageSquad/GameModes/MSGameMode.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGESQUAD_API AMSGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+};
---
+++ Source/MageSquad/GameStates/MSGameState.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "MSGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGESQUAD_API AMSGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
