a02f54ca74042c74d2b6cab079b468319ff37098
Limhs <limhs991013@gmail.com>
2025-12-16 10:01:10 +0900

    CHG : 테스트용 코드 다시 수정

--- Source/MageSquad/Enemy/MSBaseEnemy.cpp
+++ Source/MageSquad/Enemy/MSBaseEnemy.cpp
@@ -13,7 +13,7 @@ AMSBaseEnemy::AMSBaseEnemy()
 {
 	// Tick 비사용 설정 (사용 시, 활성화)
 	// 데칼 영향 비활성화 (사용 시, 활성화)
-	//PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = false;
 	PrimaryActorTick.bStartWithTickEnabled = false;
 	GetMesh()->bReceivesDecals = false;
 	
