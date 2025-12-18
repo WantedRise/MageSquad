bcdf08bf4e882ba7297e061822f074593dd1b7b9
Sechan <scpak1029@gmail.com>
2025-12-18 17:04:27 +0900

    NEW: 범위공격 행동 코드 추가

---
+++ Source/MageSquad/Actors/Projectile/Behaviors/MSProjectileBehavior_AoE.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Projectile/Behaviors/MSProjectileBehavior_AoE.h"
+
---
+++ Source/MageSquad/Actors/Projectile/Behaviors/MSProjectileBehavior_AoE.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Projectile/Behaviors/MSProjectileBehaviorBase.h"
+#include "MSProjectileBehavior_AoE.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGESQUAD_API UMSProjectileBehavior_AoE : public UMSProjectileBehaviorBase
+{
+	GENERATED_BODY()
+	
+};
