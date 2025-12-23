4c67a7ec1ef176c3a35f0623ec5689315d0d82cc
Limhs <limhs991013@gmail.com>
2025-12-23 15:26:03 +0900

    CHG : Enemy Collision false 타이밍 변경

--- Source/MageSquad/AbilitySystem/GA/Enemy/MSGA_EnemyDead.cpp
+++ Source/MageSquad/AbilitySystem/GA/Enemy/MSGA_EnemyDead.cpp
@@ -40,6 +40,7 @@ void UMSGA_EnemyDead::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 		EnemyDeadTask->ReadyForActivation();
 		UE_LOG(LogTemp, Warning, TEXT("[%s] Enemy Dead Ability Being"), *GetAvatarActorFromActorInfo()->GetName())
 		
+		Owner->SetActorEnableCollision(false);
 	}
 }
 
