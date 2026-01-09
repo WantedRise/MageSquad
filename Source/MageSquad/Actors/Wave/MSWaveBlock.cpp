0b790655c58c39b9f8129269e8918c36d9ac14c4
Limhs <limhs991013@gmail.com>
2026-01-09 18:53:30 +0900

    NEW : WaveBlock NetRelevant 변경

--- Source/MageSquad/Actors/Wave/MSWaveBlock.cpp
+++ Source/MageSquad/Actors/Wave/MSWaveBlock.cpp
@@ -11,7 +11,8 @@ AMSWaveBlock::AMSWaveBlock()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
-
+	bAlwaysRelevant = true;
+	
 	BoxCollision = CreateDefaultSubobject<UBoxComponent>("Box");
 	RootComponent = BoxCollision;
 
